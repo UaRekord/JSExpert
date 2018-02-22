5e3a09fdbe2165a261e19abbd72cda00e1755e70
UaRekord <warlock150@gmail.com>
2018-02-22 13:17:47 +0200

    допилил часы

--- clockExample/js/main.js
+++ clockExample/js/main.js
@@ -2,7 +2,6 @@
     let current = new Date(),
         date2019 = new Date('1/1/2019'); //создаю объект с датой и кладу в переменную
     
-
     function getDayOfWeek(date) { // определяю день недели
         let dayOfWeek = ['понеділок', 'вівторок', 'середа', 'четвер', "п'ятниця", 'субота', 'неділя'];
         return dayOfWeek[date - 1];
@@ -13,6 +12,26 @@
         return months[date];
     }
 
+    const calculateRest = () => Math.floor((date2019 - current) / 1000 / 60 / 60 / 24);
+
+    function getEnding() {
+        let balance = calculateRest(),
+            rest = +balance.toString().slice(-2),
+            ending = '',
+            center = 'залишилось ';
+        if ((rest == 1) || (rest % 10) == 1 && (rest != 11)) { 
+            center = 'залишився ';
+            ending = ' день';
+        }
+        else if ((rest > 1) && (rest < 5)) {
+            ending = ' дні';
+        }
+        else {
+            ending = ' днів';
+        }
+        return (center + balance + ending);
+    }
+   
     function getCurrentTime() {
         let currentTime = new Date();
         document.getElementById('time').innerHTML = `Поточний час: ${currentTime.toTimeString().substr(0, 9)}`;
@@ -20,6 +39,6 @@
 
     document.getElementById('today').innerHTML = `Сьогодні ${getDayOfWeek(current.getDay())}, ${current.getDate()} ${getCurrentMonth(current.getMonth())} ${current.getFullYear()} року`;
     setInterval(getCurrentTime, 1000); 
-    document.getElementById('rest').innerHTML = `До 2019 року залишилось ${Math.floor((date2019 - current) / 1000 / 60 / 60 / 24)} днів`;
+    document.getElementById('rest').innerHTML = `До 2019 року ${getEnding()}`;
     
 }())
